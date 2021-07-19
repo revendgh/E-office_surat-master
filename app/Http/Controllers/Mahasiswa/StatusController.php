b58378b0dacc2c529058e3c1c042e65f960610ea
Reynaldi Yulizar <darkness.yulizar@gmail.com>
2021-07-20 00:11:05 +0800

    asd

--- app/Http/Controllers/Mahasiswa/StatusController.php
+++ app/Http/Controllers/Mahasiswa/StatusController.php
@@ -4,8 +4,14 @@ namespace App\Http\Controllers\Mahasiswa;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\E_surat\Surat;
+use Auth;
 
 class StatusController extends Controller
 {
-    //
+    public function index()
+    {
+        $items = Surat::latest('updated_at')->where('id_users', Auth::user()->id)->get();
+        return view('mahasiswa.status.index', compact('items'));
+    }
 }
--- resources/views/mahasiswa/status/index.blade.php
+++ resources/views/mahasiswa/status/index.blade.php
@@ -1,59 +1,52 @@
-@extends('admin.default')
+@extends('mahasiswa.default')
 
 @section('page-header')
-    Users <small>{{ trans('app.manage') }}</small>
+    Status <small>surat</small>
 @endsection
 
 @section('content')
-
-    <div class="mB-20">
-        <a href="{{ route(ADMIN . '.users.create') }}" class="btn btn-info">
-            {{ trans('app.add_button') }}
-        </a>
-    </div>
-
 
     <div class="bgc-white bd bdrs-3 p-20 mB-20">
         <div class="table-responsive">
             <table id="dataTable" class="table table-striped table-bordered" cellspacing="0" width="100%">
                 <thead>
                     <tr>
-                        <th>Name</th>
-                        <th>Email</th>
-                        <th>Actions</th>
+                        <th>Jenis Surat</th>
+                        <th>Tanggal Pengajuan</th>
+                        <th>Status Surat</th>
                     </tr>
                 </thead>
 
                 <tfoot>
                     <tr>
-                        <th>Name</th>
-                        <th>Email</th>
-                        <th>Actions</th>
+                        <th>Jenis Surat</th>
+                        <th>Tanggal Pengajuan</th>
+                        <th>Status Surat</th>
                     </tr>
                 </tfoot>
 
                 <tbody>
                     @foreach ($items as $item)
                         <tr>
-                            <td><a href="{{ route(ADMIN . '.users.edit', $item->id) }}">{{ $item->name }}</a></td>
-                            <td>{{ $item->email }}</td>
+                            <td>{{ $item->nama_surat }}</td>
+                            <td>{{ $item->created_at }}</td>
                             <td>
-                                <ul class="list-inline">
-                                    <li class="list-inline-item">
-                                        <a href="{{ route(ADMIN . '.users.edit', $item->id) }}" title="{{ trans('app.edit_title') }}" class="btn btn-primary btn-sm"><span class="ti-pencil"></span></a></li>
-                                    <li class="list-inline-item">
-                                        {!! Form::open([
-                                            'class'=>'delete',
-                                            'url'  => route(ADMIN . '.users.destroy', $item->id),
-                                            'method' => 'DELETE',
-                                            ])
-                                        !!}
-
-                                            <button class="btn btn-danger btn-sm" title="{{ trans('app.delete_title') }}"><i class="ti-trash"></i></button>
-
-                                        {!! Form::close() !!}
-                                    </li>
-                                </ul>
+                                @if($item->status_surat == 0)
+                                <mark style="background-color: black; color: white;">
+                                            Tahap verifikasi
+                                @elseif($item->status_surat == 1 || $item->status_surat == 5)
+                                <mark style="background-color: red; color: white;">
+                                            Ditolak
+                                @elseif($item->status_surat == 2 || $item->status_surat == 6)
+                                <mark style="background-color: yellow; color: black;">
+                                            Terverifikasi
+                                @elseif($item->status_surat == 3 || $item->status_surat == 7)
+                                <mark style="background-color: green; color: white;">
+                                            Selesai
+                                @elseif($item->status_surat == 4)
+                                <mark style="background-color: #0398dc; color: white;">
+                                            Diteruskan ke Jurusan
+                                @endif
                             </td>
                         </tr>
                     @endforeach
